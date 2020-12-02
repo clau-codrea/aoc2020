7ebae232127956d942e7a296002d350a7739b496
Claudiu Codrea <claudiu.codrea@posteo.net>
2020-12-02 13:26:48 +0200

    add day 2, part 2

--- d2/passwords.py
+++ d2/passwords.py
@@ -1,20 +1,24 @@
 import sys
 import re
 
-def is_valid(match):
-    lower = int(match.group('lower'))
-    upper = int(match.group('upper'))
-    letter, password = match.group('letter', 'password')
 
-    if lower <= password.count(letter) <= upper:
-        return True
+def is_valid(match):
+    lower = int(match.group("lower"))
+    upper = int(match.group("upper"))
+    letter, password = match.group("letter", "password")
+
+    if 0 < lower < upper <= len(password):
+        return (password[lower - 1] == letter) is not (password[upper - 1] == letter)
 
     return False
-    
-def main():
-    file_name = sys.argv[1] 
 
-    pattern = re.compile('\A(?P<lower>[0-9]+)-(?P<upper>[0-9]+)\s(?P<letter>[a-z]):\s(?P<password>[a-z]+)\s*\Z')
+
+def main():
+    file_name = sys.argv[1]
+
+    pattern = re.compile(
+        "\A(?P<lower>[0-9]+)-(?P<upper>[0-9]+)\s(?P<letter>[a-z]):\s(?P<password>[a-z]+)\s*\Z"
+    )
 
     valid = 0
     with open(file_name) as passwords_file:
@@ -23,8 +27,8 @@ def main():
             if match and is_valid(match):
                 valid += 1
 
-    print(f'Valid passwords: {valid}')
+    print(f"Valid passwords: {valid}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
