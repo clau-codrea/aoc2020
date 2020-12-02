702b207e2e54d3d3670db14f1f374f3e5c0ba0b6
Claudiu Codrea <claudiu.codrea@posteo.net>
2020-12-02 21:18:30 +0200

    add day1, part 2

--- d1/expenses.py
+++ d1/expenses.py
@@ -9,10 +9,16 @@ def main(expenses_file_name):
         for line in expenses_file:
             expenses.add(int(line))
 
-    for expense in expenses:
-        if 2020 - expense in expenses:
-            print(f"{expense} x {2020 - expense} = {expense * (2020 - expense)}")
-            return
+    for first_expense in expenses:
+        for second_expense in expenses:
+            if first_expense != second_expense:
+                third_expense = 2020 - first_expense - second_expense
+                if third_expense in expenses:
+                    product = first_expense * second_expense * third_expense
+                    print(
+                        f"{first_expense} x {second_expense} x {third_expense} = {product}"
+                    )
+                    return
 
 
 if __name__ == "__main__":
