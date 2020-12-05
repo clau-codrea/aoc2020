23a0422fc8be2289d311a96b42bef2132392ccb4
Claudiu Codrea <claudiu.codrea@posteo.net>
2020-12-05 19:42:22 +0200

    add day 5, part 2

--- d5/boarding.py
+++ d5/boarding.py
@@ -6,21 +6,22 @@ def follow_directions(directions, count):
     lower, upper = 0, count - 1
     for direction in directions:
         if direction:
-            lower = (lower + upper) // 2 + 1    
+            lower = (lower + upper) // 2 + 1
         else:
             upper = (lower + upper) // 2
 
-    if lower == upper:
-        return lower
-    else:
-        print('FUCK!!!')
+    return lower
 
 
 def find_seat(boarding_pass, Seat):
-    row_directions = [0 if direction == 'F' else 1 for direction in boarding_pass[:7]]
-    column_directions = [0 if direction =='L' else 1 for direction in boarding_pass[7:]]
+    row_directions = [0 if direction == "F" else 1 for direction in boarding_pass[:7]]
+    column_directions = [
+        0 if direction == "L" else 1 for direction in boarding_pass[7:]
+    ]
 
-    return Seat(follow_directions(row_directions, 128), follow_directions(column_directions, 8))
+    return Seat(
+        follow_directions(row_directions, 128), follow_directions(column_directions, 8)
+    )
 
 
 def compute_id(seat):
@@ -33,11 +34,29 @@ def main(boarding_file_path):
         for line in boarding_file:
             passes.append(line.strip())
 
-    Seat = namedtuple('Point', ['row', 'column'])
+    Seat = namedtuple("Point", ["row", "column"])
 
-    highest_id = max(compute_id(find_seat(boarding_pass, Seat)) for boarding_pass in passes)
-    print(f'Highest seat ID: {highest_id}')
+    seat_ids = [compute_id(find_seat(boarding_pass, Seat)) for boarding_pass in passes]
+    real_first, real_last = min(seat_ids), max(seat_ids)
+
+    last_seat, last_front_missing, first_back_missing = (
+        128 * 8 - 1,
+        real_first - 1,
+        real_last + 1,
+    )
+
+    front_missing_sum = last_front_missing * (last_front_missing + 1) // 2
+    back_missing_sum = (
+        first_back_missing * (last_seat - first_back_missing + 1)
+        + (last_seat - first_back_missing) * (last_seat - first_back_missing + 1) // 2
+    )
+    total_sum = last_seat * (last_seat + 1) // 2
+
+    real_total_sum = total_sum - front_missing_sum - back_missing_sum
+    seat_id = real_total_sum - sum(seat_ids)
+
+    print(f"Seat ID: {seat_id}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(sys.argv[1])
