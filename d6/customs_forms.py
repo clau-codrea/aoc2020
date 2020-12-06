0fd912baa4a126e4a84b1cddad65a5e370be085f
Claudiu Codrea <claudiu.codrea@posteo.net>
2020-12-06 12:51:42 +0200

    add day 6, part 2

--- d6/customs_forms.py
+++ d6/customs_forms.py
@@ -1,24 +1,26 @@
 import sys
+from collections import Counter
 
 
 def main(forms_file_path):
     total = 0
     with open(forms_file_path) as forms_file:
-        form = set()
+        form, people = Counter(), 0
         for line in forms_file:
             questions = line.rstrip()
 
             if not questions:
-                total += len(form)
-                form = set()
+                total += sum(1 if form[question] == people else 0 for question in form)
+                form, people = Counter(), 0
+            else:
+                people += 1
+                for question in questions:
+                    form[question] += 1
 
-            for question in questions:
-                form.add(question)
+        total += sum(1 if form[question] == people else 0 for question in form)
 
-        total+= len(form)
-
-    print(f'Total: {total}')
+    print(f"Total: {total}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(sys.argv[1])
