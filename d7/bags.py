0f753178dd1b4f612129debc819af0312d128cb2
Claudiu Codrea <claudiu.codrea@posteo.net>
2020-12-07 12:29:01 +0200

    add day 7 part 2

--- d7/bags.py
+++ d7/bags.py
@@ -3,12 +3,12 @@ import re
 
 
 def read_rules(bags_file):
-    parents = {}
+    contains = {}
 
     line_pattern = re.compile(
         "\A(?P<parent_color>[a-z]+ [a-z]+)(?: bags contain )(?P<children_colors>.+)\s\Z"
     )
-    bag_pattern = re.compile("\A[0-9]+ (?P<color>[a-z]+ [a-z]+) bag")
+    bag_pattern = re.compile("(?P<number>\A[0-9]+) (?P<color>[a-z]+ [a-z]+) bag")
     for line in bags_file:
         line_match = line_pattern.match(line)
         if line_match:
@@ -19,24 +19,24 @@ def read_rules(bags_file):
                 bag_match = bag_pattern.match(bag)
                 if bag_match:
                     child_color = bag_match.group("color")
-                    if child_color in parents:
-                        parents[child_color].add(parent_color)
+                    child_number = int(bag_match.group("number"))
+                    if parent_color in contains:
+                        contains[parent_color][child_color] = child_number
                     else:
-                        parents[child_color] = {parent_color}
+                        contains[parent_color] = {child_color: child_number}
 
-    return parents
+    return contains
 
 
-def all_ancestors(bag, bag_rules):
-    ancestors = set()
+def get_contents(bag, bag_rules):
+    contents = 0
     if bag not in bag_rules:
-        return ancestors
+        return contents
 
-    for parent in bag_rules[bag]:
-        ancestors.add(parent)
-        ancestors.update(all_ancestors(parent, bag_rules))
+    for child in bag_rules[bag]:
+        contents += bag_rules[bag][child] * (1 + get_contents(child, bag_rules))
 
-    return ancestors
+    return contents
 
 
 def main(bags_file_path):
@@ -44,9 +44,9 @@ def main(bags_file_path):
         bag_rules = read_rules(bags_file)
 
     bag = "shiny gold"
-    bags_containing = all_ancestors(bag, bag_rules)
+    contents = get_contents(bag, bag_rules)
 
-    print(f"Bags that can contain {bag}: {len(bags_containing)}")
+    print(f"Bags inside {bag}: {contents}")
 
 
 if __name__ == "__main__":
