97a7e082250a2abc6784d76dd7c611499876219d
Claudiu Codrea <claudiu.codrea@posteo.net>
2020-12-10 13:04:27 +0200

    day 10 part 2 slow

--- d10/adapters.py
+++ d10/adapters.py
@@ -1,25 +1,50 @@
 import sys
 
 
-def count_differences(adapters):
-    differences = {1: 0, 3: 1}
-    previous = 0
-    for adapter in adapters:
-        differences[adapter - previous] += 1
-        previous = adapter
+def count_arrangements(adapters):
+    adapters.sort()
 
-    return differences
+    arrangements = 0
+    mask = [None] * (len(adapters) - 1) + [1]
+
+    def check_differences(mask, index):
+        if not mask[index]:
+            previous = 0
+            for previous_index in range(index - 1, -1, -1):
+                if mask[previous_index]:
+                    previous = adapters[previous_index]
+                    break
+
+            if adapters[index + 1] - previous <= 3:
+                return True
+
+            return False
+
+        return True
+
+    def generate_masks(index):
+        nonlocal arrangements
+
+        if index == len(adapters) - 1:
+            arrangements += 1
+        else:
+            for choice in range(2):
+                mask[index] = choice
+                if not check_differences(mask, index):
+                    continue
+                generate_masks(index + 1)
+
+    generate_masks(0)
+
+    return arrangements
 
 
 def main(adapters_file_path):
-    adapters = []
     with open(adapters_file_path) as adapters_file:
-        for line in adapters_file:
-            adapters.append(int(line))
+        adapters = [int(line) for line in adapters_file]
 
-    differences = count_differences(sorted(adapters))
-    jolts = differences[1] * differences[3]
-    print(f"jolts: {jolts}")
+    arrangements = count_arrangements(adapters)
+    print(f"arrangements: {arrangements}")
 
 
 if __name__ == "__main__":
