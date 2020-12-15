a4599fc5873f97ef16dd6279fa8d93b702460bfa
Claudiu Codrea <claudiu.codrea@posteo.net>
2020-12-16 00:13:08 +0200

    day 15 still slow

--- d15/recite.py
+++ d15/recite.py
@@ -1,21 +1,20 @@
 def recite(numbers, last):
     previous_position = {}
-    for index, number in enumerate(reversed(numbers)):
-        previous_position[number] = index
+    for index, number in enumerate(numbers):
+        previous_position[number] = -index
 
     number = numbers[-1]
     turn = len(numbers)
     while turn != last:
+        print(turn, number)
         if number not in previous_position:
-            previous_position[number] = 0
+            previous_position[number] = -(turn - 1)
             number = 0
         else:
-            aux = previous_position[number]
-            previous_position[number] = 0
+            aux = (turn - 1) + previous_position[number]
+            previous_position[number] = -(turn - 1)
             number = aux
 
-        for counter in previous_position:
-            previous_position[counter] += 1
         turn += 1
 
     return number
