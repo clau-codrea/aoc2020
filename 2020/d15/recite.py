46e22c57d476bfc50e44ca4fedc9b955f191c8cf
Claudiu Codrea <claudiu.codrea@posteo.net>
2020-12-24 00:28:30 +0200

    add day 15 part 2

--- 2020/d15/recite.py
+++ 2020/d15/recite.py
@@ -4,15 +4,14 @@ def recite(numbers, last):
         previous_position[number] = -index
 
     number = numbers[-1]
-    turn = len(numbers)
-    while turn != last:
-        print(turn, number)
+    turn = len(numbers) - 1
+    while turn < last - 1:
         if number not in previous_position:
-            previous_position[number] = -(turn - 1)
+            previous_position[number] = -turn
             number = 0
         else:
-            aux = (turn - 1) + previous_position[number]
-            previous_position[number] = -(turn - 1)
+            aux = turn + previous_position[number]
+            previous_position[number] = -turn
             number = aux
 
         turn += 1
