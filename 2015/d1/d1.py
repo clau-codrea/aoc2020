c7d43e97b147b7b0362a3d50ec036b6271374e58
Claudiu Codrea <claudiu.codrea@posteo.net>
2021-01-02 19:34:11 +0200

    add 2015 day 1 part 2

--- 2015/d1/d1.py
+++ 2015/d1/d1.py
@@ -1,11 +1,19 @@
-def find_floor(instructions):
-    return sum(1 if instruction == '(' else -1 for instruction in instructions)
+BASEMENT = -1
+
+def reach_basement(instructions):
+    floor = 0
+    for index, instruction in enumerate(instructions):
+        direction = 1 if instruction == '(' else -1
+        floor += direction
+        if floor == BASEMENT:
+            return index + 1
+
 
 def main():
     with open('input') as input_file:
         instructions = input_file.readline().rstrip()
 
-    floor = find_floor(instructions)
-    print(floor)
+    instruction_position = reach_basement(instructions)
+    print(instruction_position)
 
 main()
